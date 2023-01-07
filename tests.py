0d843bab57cce7ecd8fc850d49340041b325efe7
Paul <paulflorentin.pf@gmail.com>
2023-01-07 20:13:00 +0200

    some tests

--- tests.py
+++ tests.py
@@ -11,16 +11,33 @@ from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 
 class TestLogin:
+    @classmethod
     def setUp(self):
         if os.path.exists("geckodriver.log") and os.path.exists("geckodriver.exe"):
             pass
         else:
-            driver = webdriver.Firefox(
+            self.driver = webdriver.Firefox(
                 service=FirefoxService(GeckoDriverManager().install())
             )
+        self.driver.get("http://localhost/prestashopSite/")
+        self.driver.maximize_window()
 
-    # tests here
 
+    def test_page(self):
+        # self.setUp()
+        assert self.driver.title == "General Store"
+        # self.tearDown()
+
+    # def test_login(self):
+    #     self.driver.get('http://localhost/prestashopSite/')
+
+    @classmethod
     def tearDown(self):
         self.driver.quit()
         # to be implemented a method to clean the message data base, for testing purposes
+
+@pytest.fixture
+def selenium(selenium):
+    selenium.implicitly_wait(10)
+    selenium.maximize_window()
+    return selenium

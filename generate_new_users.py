2671b761d6de13fe51af9cebcbabb33e29f7e1de
Paul <paulflorentin.pf@gmail.com>
2023-01-09 09:20:38 +0200

    generate new users wip

---
+++ generate_new_users.py
@@ -0,0 +1,22 @@
+import random
+
+
+def get_genders():
+    genders = ['Mr.', 'Mrs.']
+    return random.choice(genders)
+
+
+def generate_email():
+    pass
+
+
+def generate_password():
+    pass
+
+
+def generate_first_last_name():
+    pass
+
+
+def user_dump():
+    pass

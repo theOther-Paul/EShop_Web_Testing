99890b65beb192771bd9158840458bace1cc9aa1
Paul <paulflorentin.pf@gmail.com>
2023-01-10 08:43:21 +0200

    generate new users wip

--- generate_new_users.py
+++ generate_new_users.py
@@ -1,4 +1,6 @@
 import random
+import requests
+import json
 
 
 def get_genders():
@@ -20,3 +22,28 @@ def generate_first_last_name():
 
 def user_dump():
     pass
+
+
+# needs debugging
+def api_connection():
+    global data
+    response = requests.get('https://randomuser.me/api/?inc=gender,name,nat,login')
+    if response.status_code == 200:
+        print("connection successfully created")
+        data = response.json()
+        print(data)
+        # raw = json.loads(data)
+    elif response.status_code == 404:
+        print("Unable to reach URL.")
+    else:
+        print("Unable to connect API or retrieve data.")
+    for record in data:
+        print(f"Gender: {record['gender']},\n Name : {record['first'] + record['last']}, \n Title : {record['title']}, \n Email : {record['email']}, \nUsername : {record['username']}, \n Password : {record['password']}")
+
+
+def main():
+    api_connection()
+
+
+if __name__ == '__main__':
+    main()

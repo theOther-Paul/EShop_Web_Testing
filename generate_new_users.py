d8a89e170dda9da3fd0dcd0b58923dc9767f377e
Paul <paulflorentin.pf@gmail.com>
2023-01-11 08:52:40 +0200

    done the generate_new_users.py. json file as sample

--- generate_new_users.py
+++ generate_new_users.py
@@ -1,49 +1,56 @@
-import random
 import requests
 import json
+from enum import Enum
+import os
 
 
-def get_genders():
-    genders = ['Mr.', 'Mrs.']
-    return random.choice(genders)
+# will take care of errors and exceptions
+class ErrorMessage(Enum):
+    API_CONNECTION_ERROR = "Unable to connect to API"
+    RETRIEVAL_ERROR = "Unable to retrieve data"
+    STORAGE_ERROR = "Function can't save file"
 
 
-def generate_email():
-    pass
-
-
-def generate_password():
-    pass
-
-
-def generate_first_last_name():
-    pass
-
-
-def user_dump():
-    pass
-
-
-# needs debugging
 def api_connection():
-    global data
-    response = requests.get('https://randomuser.me/api/?inc=gender,name,nat,login')
+    response = requests.get('https://randomuser.me/api/?inc=gender,name,nat,login,email')
     if response.status_code == 200:
-        print("connection successfully created")
         data = response.json()
-        print(data)
-        # raw = json.loads(data)
+        try:
+            with open('api_response/user_data.json', 'w+') as output_data:
+                json.dump(data, output_data)
+        except ErrorMessage.STORAGE_ERROR:
+            return ErrorMessage.STORAGE_ERROR.value
+        return data
     elif response.status_code == 404:
-        print("Unable to reach URL.")
+        return ErrorMessage.API_CONNECTION_ERROR.value
     else:
-        print("Unable to connect API or retrieve data.")
-    for record in data:
-        print(f"Gender: {record['gender']},\n Name : {record['first'] + record['last']}, \n Title : {record['title']}, \n Email : {record['email']}, \nUsername : {record['username']}, \n Password : {record['password']}")
+        return ErrorMessage.RETRIEVAL_ERROR.value
 
 
-def main():
-    api_connection()
+def get_gender_title(json_file):
+    with open(f'api_response/{json_file}') as source:
+        data = json.load(source)
+        for record in data["results"]:
+            return record['name']['title']
 
 
-if __name__ == '__main__':
-    main()
+def get_email(json_file):
+    with open(f'api_response/{json_file}') as source:
+        data = json.load(source)
+        return ''.join([record['email'] for record in data['results']])
+
+
+def generate_password(json_file):
+    with open(f'api_response/{json_file}') as source:
+        data = json.load(source)
+        return ''.join([record['login']['password'] for record in data['results']])
+
+
+def get_first_last_name(json_file):
+    with open(f'api_response/{json_file}') as source:
+        data = json.load(source)
+        return ''.join(record['name']['first'] + ' ' + record['name']['last'] for record in data['results'])
+
+
+def drop_user_data(json_file):
+    os.remove(f'api_response/{json_file}')
